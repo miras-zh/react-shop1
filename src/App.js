530ded825626138048417fe300e2cca7e5201411
mikos8 <mikos.zh8@gmail.com>
2021-08-06 13:42:23 +0600

    add fetch / backend

--- src/App.js
+++ src/App.js
@@ -1,61 +1,83 @@
 /* eslint-disable no-unused-vars */
-import Card from './components/Card/Card';
+import React from "react";
+import Card from "./components/Card/Card";
 /* eslint-disable jsx-a11y/alt-text */
-import Header from './components/Header';
-import Korzina from './components/Korzina';
+import Header from "./components/Header";
+import Korzina from "./components/Korzina";
 
-const arr = [
-	{
-		name: 'Мужские кроссовки Nike Blazer Mid Suede',
-		price: '15 000 T',
-		url: '/image/1.png',
-	},
-	{
-		name: 'Мужские Кроссовки Nike Air Max 270',
-		price: '14 000 T',
-		url: '/image/2.png',
-	},
-	{
-		name: 'Мужские Кроссовки Nike Blazer Mid Suede',
-		price: '18 000 T',
-		url: '/image/3.png',
-	},
-	{
-		name: 'Кроссовки Puma X Aka Boku Future Rider',
-		price: '20 500 T',
-		url: '/image/4.png',
-	},
-];
-
+const arr = [];
+/*
+{
+    name: "Мужские кроссовки Nike Blazer Mid Suede",
+    price: "15 000 T",
+    url: "/image/1.png",
+  },
+  {
+    name: "Мужские Кроссовки Nike Air Max 270",
+    price: "14 000 T",
+    url: "/image/2.png",
+  },
+  {
+    name: "Мужские Кроссовки Nike Blazer Mid Suede",
+    price: "18 000 T",
+    url: "/image/3.png",
+  },
+  {
+    name: "Кроссовки Puma X Aka Boku Future Rider",
+    price: "20 500 T",
+    url: "/image/4.png",
+  },
+*/
 function App() {
-	return (
-		<div className="wrapper clear">
-			<Korzina />
-			<Header />
-			<div className="content p-40 ">
-				<div className="d-flex align-center mb-40 justify-between">
-					<h1>Все кроссовки</h1>
-					<div className="search-block">
-						<img src="/image/search.svg" alt="search" />
-						<input type="text" placeholder="Поиск .." />
-					</div>
-				</div>
-				<div className="d-flex">
-					{arr.map((val) => (
-						<Card
-							title={val.name}
-							price={val.price}
-							url={val.url}
-							onClick={() => {
-								console.log('', val);
-							}}
-							onFavorite={()=>{console.log('favorite click')}}
-						/>
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  const [items, setItems] = React.useState([]);
+  const [cartOpened, setCartOpened] = React.useState(false);
+  fetch("https://610ce13a66dd8f0017b76f02.mockapi.io/items")
+    .then((res) => {
+      return res.json();
+    })
+    .then((res) => {
+      setItems(res);
+    });
+  return (
+    <div className="wrapper clear">
+      {cartOpened ? (
+        <Korzina
+          onClickClose={() => {
+            setCartOpened(false);
+          }}
+        />
+      ) : null}
+      <Header
+        onClickCart={() => {
+          setCartOpened(true);
+        }}
+      />
+      <div className="content p-40 ">
+        <div className="d-flex align-center mb-40 justify-between">
+          <h1>Все кроссовки</h1>
+          <div className="search-block">
+            <img src="/image/search.svg" alt="search" />
+            <input type="text" placeholder="Поиск .." />
+          </div>
+        </div>
+        <div className="d-flex flex-wrap">
+          {items.map((val) => (
+            <Card
+              title={val.name}
+              price={val.price}
+              url={val.url}
+              onClick={() => {
+                console.log("", val);
+              }}
+              onFavorite={() => {
+                console.log("favorite click");
+              }}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,27 +1,42 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable no-unused-vars */
-import style from './Card.module.scss';
+import React from "react";
+import style from "./Card.module.scss";
 
 function Card(props) {
-	return (
-		<div className={style.card}>
-			<div className='styles.card-favorite' onClick={props.onFavorite}>
-				<img src='/image/heart-unlike.svg' alt='unliked'/>
-			</div>
-			<img width={133} height={112} src={props.url} alt="tovar" />
-			<h5>{props.title}</h5>
-			<div className="d-flex justify-between align-center">
-				<div className="d-flex flex-column">
-					<span>Цена:</span>
-					<b>{props.price}</b>
-				</div>
-				{/* <button className="button" onClick={btnclickPlus}> */}
-				<button className="button" onClick={props.onClick}>
-					<img width={11} height={11} src="/image/plus.svg" alt="Plus" />
-				</button>
-			</div>
-		</div>
-	);
+  const [isAdded, setIsAdded] = React.useState(false);
+
+  const onClickPlus = () => {
+    isAdded ? setIsAdded(false) : setIsAdded(true);
+  };
+
+  React.useEffect(() => {
+    console.log("change");
+  }, [isAdded]);
+
+  return (
+    <div className={style.card}>
+      <div className="styles.card-favorite" onClick={props.onFavorite}>
+        <img src="/image/heart-unlike.svg" alt="unliked" />
+      </div>
+      <img width={133} height={112} src={props.url} alt="tovar" />
+      <h5>{props.title}</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>{props.price}</b>
+        </div>
+        <img
+          className={style.plus}
+          width={11}
+          height={11}
+          src={!isAdded ? "/image/plus.svg" : "/image/set.svg"}
+          alt="Plus"
+          onClick={onClickPlus}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Card;
